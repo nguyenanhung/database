f4200cabfcae0528c7fc860b7f770744e038c22e
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-16 16:11:45 +0700

    Update Base Model

--- src/Model/BaseModel.php
+++ src/Model/BaseModel.php
@@ -12,37 +12,68 @@ namespace nguyenanhung\MyDatabase\Model;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
+use nguyenanhung\MyDebug\Debug;
 use nguyenanhung\MyDatabase\Interfaces\ProjectInterface;
+use nguyenanhung\MyDatabase\Interfaces\BaseModelInterface;
 
-class BaseModel implements ProjectInterface
+/**
+ * Class BaseModel
+ *
+ * @package   nguyenanhung\MyDatabase\Model
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+class BaseModel implements ProjectInterface, BaseModelInterface
 {
+    /** @var object Đối tượng khởi tạo dùng gọi đến Class Debug \nguyenanhung\MyDebug\Debug */
+    protected $debug;
+    /** @var array|null Mảng dữ liệu chứa thông tin database cần kết nối tới */
     protected $db;
+    /** @var string|null Bảng cần lấy dữ liệu */
     protected $table;
+    /** @var object Đối tượng khởi tạo dùng gọi đến Class Capsule Manager \Illuminate\Database\Capsule\Manager */
     protected $capsule;
-    public    $primaryKey = 'id';
+    /** @var bool Cấu hình trạng thái Debug, TRUE nếu bật, FALSE nếu tắt */
+    public $debugStatus = FALSE;
+    /**
+     * @var null|string Cấu hình Level Debug
+     * @see https://github.com/nguyenanhung/my-debug/blob/master/src/Interfaces/DebugInterface.php
+     */
+    public $debugLevel = NULL;
+    /** @var null|bool|string Cấu hình thư mục lưu trữ Log, VD: /your/to/path */
+    public $debugLoggerPath = NULL;
+    /** @var null|string Cấu hình File Log, VD: Log-2018-10-15.log | Log-date('Y-m-d').log */
+    public $debugLoggerFilename = NULL;
+    /** @var string Primary Key Default */
+    public $primaryKey = 'id';
 
     /**
      * BaseModel constructor.
      */
     public function __construct()
     {
-        $this->db      = [
-            'driver'    => 'mysql',
-            'host'      => '127.0.0.1',
-            'database'  => 'vas_content',
-            'username'  => 'root',
-            'password'  => '',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ];
-        $this->capsule = new Capsule;
-        $this->capsule->addConnection($this->db);
-        $this->capsule->setEventDispatcher(new Dispatcher(new Container));
-        // Make this Capsule instance available globally via static methods... (optional)
-        $this->capsule->setAsGlobal();
-        // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-        $this->capsule->bootEloquent();
+        $this->debug = new Debug();
+        if ($this->debugStatus === TRUE) {
+            $this->debug->setDebugStatus($this->debugStatus);
+            if ($this->debugLevel) {
+                $this->debug->setGlobalLoggerLevel($this->debugLevel);
+            }
+            if ($this->debugLoggerPath) {
+                $this->debug->setLoggerPath($this->debugLoggerPath);
+            }
+            if (empty($this->debugLoggerFilename)) {
+                $this->debugLoggerFilename = 'Log-' . date('Y-m-d') . '.log';
+            }
+            $this->debug->setLoggerSubPath(__CLASS__);
+            $this->debug->setLoggerFilename($this->debugLoggerFilename);
+        }
+    }
+
+    /**
+     * BaseModel destructor.
+     */
+    public function __destruct()
+    {
     }
 
     /**
@@ -57,6 +88,24 @@ class BaseModel implements ProjectInterface
     public function getVersion()
     {
         return self::VERSION;
+    }
+
+    /**
+     * Hàm khởi tạo kết nối đến Cơ sở dữ liệu
+     *
+     * Sử dụng đối tượng DB được truyền từ bên ngoài vào
+     *
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 10/16/18 15:47
+     *
+     */
+    public function connection()
+    {
+        $this->capsule = new Capsule;
+        $this->capsule->addConnection($this->db);
+        $this->capsule->setEventDispatcher(new Dispatcher(new Container));
+        $this->capsule->setAsGlobal();
+        $this->capsule->bootEloquent();
     }
 
     /**
@@ -107,9 +156,11 @@ class BaseModel implements ProjectInterface
      */
     public function countAll()
     {
-        $result = Capsule::table($this->table)->count();
+        $this->connection();
+        $db = Capsule::table($this->table);
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
 
-        return $result;
+        return $db->count();
     }
 
     /**
@@ -125,11 +176,11 @@ class BaseModel implements ProjectInterface
      */
     public function checkExists($value = '', $field = 'id')
     {
-        $result = Capsule::table($this->table)
-                         ->where($field, '=', $value)
-                         ->count();
+        $this->connection();
+        $db = Capsule::table($this->table)->where($field, '=', $value);
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
 
-        return $result;
+        return $db->count();
     }
 
     /**
@@ -151,6 +202,7 @@ class BaseModel implements ProjectInterface
      */
     public function getInfo($value = '', $field = 'id', $format = NULL)
     {
+        $this->connection();
         $format = strtolower($format);
         $db     = Capsule::table($this->table);
         if (is_array($value) && count($value) > 0) {
@@ -164,6 +216,7 @@ class BaseModel implements ProjectInterface
         } else {
             $db->where($field, '=', $value);
         }
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
         if ($format == 'result') {
             $result = $db->get();
         } else {
@@ -198,6 +251,7 @@ class BaseModel implements ProjectInterface
      */
     public function getValue($value = '', $field = 'id', $fieldOutput = '')
     {
+        $this->connection();
         $db = Capsule::table($this->table);
         if (is_array($value) && count($value) > 0) {
             foreach ($value as $f => $v) {
@@ -210,6 +264,7 @@ class BaseModel implements ProjectInterface
         } else {
             $db->where($field, '=', $value);
         }
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
         $result = $db->first();
         if (!empty($fieldOutput) && isset($result->$fieldOutput)) {
             return $result->$fieldOutput;
@@ -233,8 +288,11 @@ class BaseModel implements ProjectInterface
         if (!is_array($field)) {
             $field = [$field];
         }
-        $db     = Capsule::table($this->table);
-        $result = $db->distinct()->get($field);
+        $this->connection();
+        $db = Capsule::table($this->table);
+        $db->distinct();
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
+        $result = $db->get($field);
 
         return $result;
     }
@@ -251,7 +309,10 @@ class BaseModel implements ProjectInterface
      */
     public function add($data = [])
     {
-        $id = Capsule::table($this->table)->insertGetId($data);
+        $this->connection();
+        $db = Capsule::table($this->table);
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
+        $id = $db->insertGetId($data);
 
         return $id;
     }
@@ -269,6 +330,7 @@ class BaseModel implements ProjectInterface
      */
     public function update($data = [], $wheres = [])
     {
+        $this->connection();
         $db = Capsule::table($this->table);
         if (is_array($wheres) && count($wheres) > 0) {
             foreach ($wheres as $field => $value) {
@@ -281,6 +343,7 @@ class BaseModel implements ProjectInterface
         } else {
             $db->where($this->primaryKey, '=', $wheres);
         }
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
         $result = $db->update($data);
 
         return $result;
@@ -298,6 +361,7 @@ class BaseModel implements ProjectInterface
      */
     public function delete($wheres = [])
     {
+        $this->connection();
         $db = Capsule::table($this->table);
         if (is_array($wheres) && count($wheres) > 0) {
             foreach ($wheres as $field => $value) {
@@ -310,12 +374,9 @@ class BaseModel implements ProjectInterface
         } else {
             $db->where($this->primaryKey, '=', $wheres);
         }
+        $this->debug->info(__FUNCTION__, 'SQL Queries: ' . $db->toSql());
         $result = $db->delete();
 
         return $result;
-    }
-
-    public function __destruct()
-    {
     }
 }
