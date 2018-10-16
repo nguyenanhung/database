a516455a23f43b54b512e16e042f39142a887ddd
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-16 12:04:50 +0700

    Update DB

---
+++ src/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project database.
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/16/18
+ * Time: 11:22
+ */
+
+namespace nguyenanhung\MyDatabase;
+
+class Test
+{
+
+}
