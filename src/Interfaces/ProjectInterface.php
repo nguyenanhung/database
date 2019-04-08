e56be8106fec06f2793155388af4c7c2c2a51bc8
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2019-04-08 10:07:07 +0700

    official release

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -18,8 +18,8 @@ namespace nguyenanhung\MyDatabase\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION                = '0.1.11';
-    const LAST_MODIFIED          = '2019-04-07';
+    const VERSION                = '1.0';
+    const LAST_MODIFIED          = '2019-04-08';
     const AUTHOR_NAME            = 'Hung Nguyen';
     const AUTHOR_EMAIL           = 'dev@nguyenanhung.com';
     const PROJECT_NAME           = 'My Database';
