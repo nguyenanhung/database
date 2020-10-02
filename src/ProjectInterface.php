d31ca20a7a1f84d20ca56eda82b2b7f3d31b9cff
nguyenanhung <info@nguyenanhung.com>
2020-10-02 17:53:01 +0700

    Update lại 5.4 cho illuminate

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -18,8 +18,8 @@ namespace nguyenanhung\MyDatabase;
  */
 interface ProjectInterface
 {
-    const VERSION                = '1.0.13';
-    const LAST_MODIFIED          = '2020-10-01';
+    const VERSION                = '1.0.14';
+    const LAST_MODIFIED          = '2020-10-02';
     const AUTHOR_NAME            = 'Hung Nguyen';
     const AUTHOR_EMAIL           = 'dev@nguyenanhung.com';
     const PROJECT_NAME           = 'My Database';
