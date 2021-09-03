937252189caaa668f84eecfbd9f03cee522fef24
nguyenanhung <info@nguyenanhung.com>
2021-09-04 03:59:08 +0700

    Update

--- src/Environment.php
+++ src/Environment.php
@@ -19,7 +19,7 @@ namespace nguyenanhung\MyDatabase;
 interface Environment
 {
     const VERSION       = '2.0.6.2';
-    const LAST_MODIFIED = '2021-08-31';
+    const LAST_MODIFIED = '2021-09-04';
     const AUTHOR_NAME   = 'Hung Nguyen';
     const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
     const PROJECT_NAME  = 'My Database';
