89b8d3d40484683863f8f6d74363874a72bb1f75
nguyenanhung <info@nguyenanhung.com>
2021-09-22 01:10:21 +0700

    Add Sample Code

---
+++ sample/test/test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Project database
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/22/2021
+ * Time: 01:09
+ */
+require_once __DIR__ . '/../../vendor/autoload.php';
